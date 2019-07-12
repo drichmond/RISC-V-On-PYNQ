8b2a37f0eece5ea56ac8313d7d56575219f9161d
Dustin Richmond <dustinar@uw.edu>
2019-07-12 06:18:58 -0700

    Working Cortex M3 Overlay

--- riscvonpynq/cortexm3/tcm/cortexm3.tcl
+++ riscvonpynq/cortexm3/tcm/cortexm3.tcl
@@ -83,13 +83,13 @@ if { $bCheckIPsPassed != 1 } {
 ##################################################################
 
 
-# Hierarchical cell: cortexm3
-proc create_hier_cell_cortexm3_1 { parentCell nameHier } {
+# Hierarchical cell: processor
+proc create_hier_cell_processor { parentCell nameHier } {
 
   variable script_folder
 
   if { $parentCell eq "" || $nameHier eq "" } {
-     catch {common::send_msg_id "BD_TCL-102" "ERROR" "create_hier_cell_cortexm3_1() - Empty argument(s)!"}
+     catch {common::send_msg_id "BD_TCL-102" "ERROR" "create_hier_cell_processor() - Empty argument(s)!"}
      return
   }
 
@@ -208,10 +208,10 @@ proc create_hier_cell_cortexm3_1 { parentCell nameHier } {
 
   # Create interface connections
   connect_bd_intf_net -intf_net Conn2 [get_bd_intf_pins S_AXI_MEM] [get_bd_intf_pins psBramController/S_AXI]
-  connect_bd_intf_net -intf_net S00_AXI_1 [get_bd_intf_pins cortexm3/CM3_SYS_AXI3] [get_bd_intf_pins spAxiInterconnect/S00_AXI]
   connect_bd_intf_net -intf_net axi_interconnect_0_M00_AXI [get_bd_intf_pins spAxiInterconnect/M00_AXI] [get_bd_intf_pins spBramController/S_AXI]
   connect_bd_intf_net -intf_net axi_interconnect_0_M01_AXI [get_bd_intf_pins M_AXI] [get_bd_intf_pins spAxiInterconnect/M01_AXI]
   connect_bd_intf_net -intf_net cortexm3_CM3_CODE_AXI3 [get_bd_intf_pins cortexm3/CM3_CODE_AXI3] [get_bd_intf_pins spAxiInterconnect/S01_AXI]
+  connect_bd_intf_net -intf_net cortexm3_CM3_SYS_AXI3 [get_bd_intf_pins cortexm3/CM3_SYS_AXI3] [get_bd_intf_pins spAxiInterconnect/S00_AXI]
   connect_bd_intf_net -intf_net psBramController_BRAM_PORTA [get_bd_intf_pins psBramController/BRAM_PORTA] [get_bd_intf_pins spBram/BRAM_PORTB]
   connect_bd_intf_net -intf_net riscvBramController_BRAM_PORTA [get_bd_intf_pins spBram/BRAM_PORTA] [get_bd_intf_pins spBramController/BRAM_PORTA]
 
@@ -273,9 +273,6 @@ proc create_root_design { parentCell } {
   # Create ports
   set led_o [ create_bd_port -dir O -from 3 -to 0 led_o ]
   set pb_i [ create_bd_port -dir I -from 3 -to 0 pb_i ]
-
-  # Create instance: cortexm3
-  create_hier_cell_cortexm3_1 [current_bd_instance .] cortexm3
 
   # Create instance: irqAxiInterconnect, and set properties
   set irqAxiInterconnect [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 irqAxiInterconnect ]
@@ -1166,6 +1163,9 @@ proc create_root_design { parentCell } {
    CONFIG.PCW_WDT_WDT_IO {<Select>} \
  ] $processing_system7_0
 
+  # Create instance: processor
+  create_hier_cell_processor [current_bd_instance .] processor
+
   # Create instance: psAxiInterconnect, and set properties
   set psAxiInterconnect [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 psAxiInterconnect ]
   set_property -dict [ list \
@@ -1217,33 +1217,39 @@ proc create_root_design { parentCell } {
   connect_bd_intf_net -intf_net processing_system7_0_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins processing_system7_0/DDR]
   connect_bd_intf_net -intf_net processing_system7_0_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins processing_system7_0/FIXED_IO]
   connect_bd_intf_net -intf_net processing_system7_0_M_AXI_GP1 [get_bd_intf_pins irqAxiInterconnect/S00_AXI] [get_bd_intf_pins processing_system7_0/M_AXI_GP1]
-  connect_bd_intf_net -intf_net psAxiInterconnect_M00_AXI [get_bd_intf_pins cortexm3/S_AXI_MEM] [get_bd_intf_pins psAxiInterconnect/M00_AXI]
+  connect_bd_intf_net -intf_net psAxiInterconnect_M00_AXI [get_bd_intf_pins processor/S_AXI_MEM] [get_bd_intf_pins psAxiInterconnect/M00_AXI]
   connect_bd_intf_net -intf_net psAxiInterconnect_M01_AXI [get_bd_intf_pins psAxiInterconnect/M01_AXI] [get_bd_intf_pins subprocessorClk/s_axi_lite]
-  connect_bd_intf_net -intf_net tutorialProcessor_CM1_AXI3 [get_bd_intf_pins cortexm3/M_AXI] [get_bd_intf_pins irqAxiInterconnect/S01_AXI]
+  connect_bd_intf_net -intf_net tutorialProcessor_CM1_AXI3 [get_bd_intf_pins irqAxiInterconnect/S01_AXI] [get_bd_intf_pins processor/M_AXI]
 
   # Create port connections
-  connect_bd_net -net FCLK_CLK0 [get_bd_pins cortexm3/s_axi_aclk] [get_bd_pins irqAxiInterconnect/ACLK] [get_bd_pins irqAxiInterconnect/M00_ACLK] [get_bd_pins irqAxiInterconnect/S00_ACLK] [get_bd_pins porReset/slowest_sync_clk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins processing_system7_0/M_AXI_GP1_ACLK] [get_bd_pins psAxiInterconnect/ACLK] [get_bd_pins psAxiInterconnect/M00_ACLK] [get_bd_pins psAxiInterconnect/M01_ACLK] [get_bd_pins psAxiInterconnect/S00_ACLK] [get_bd_pins psInterruptController/s_axi_aclk] [get_bd_pins subprocessorClk/clk_in1] [get_bd_pins subprocessorClk/s_axi_aclk]
+  connect_bd_net -net FCLK_CLK0 [get_bd_pins irqAxiInterconnect/ACLK] [get_bd_pins irqAxiInterconnect/M00_ACLK] [get_bd_pins irqAxiInterconnect/S00_ACLK] [get_bd_pins porReset/slowest_sync_clk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins processing_system7_0/M_AXI_GP1_ACLK] [get_bd_pins processor/s_axi_aclk] [get_bd_pins psAxiInterconnect/ACLK] [get_bd_pins psAxiInterconnect/M00_ACLK] [get_bd_pins psAxiInterconnect/M01_ACLK] [get_bd_pins psAxiInterconnect/S00_ACLK] [get_bd_pins psInterruptController/s_axi_aclk] [get_bd_pins subprocessorClk/clk_in1] [get_bd_pins subprocessorClk/s_axi_aclk]
   connect_bd_net -net FCLK_CLK1 [get_bd_pins processing_system7_0/FCLK_CLK1] [get_bd_pins processing_system7_0/S_AXI_HP2_ACLK]
-  connect_bd_net -net S00_ARESETN_1 [get_bd_pins cortexm3/s_axi_aresetn] [get_bd_pins irqAxiInterconnect/M00_ARESETN] [get_bd_pins irqAxiInterconnect/S00_ARESETN] [get_bd_pins porReset/peripheral_aresetn] [get_bd_pins psAxiInterconnect/M00_ARESETN] [get_bd_pins psAxiInterconnect/M01_ARESETN] [get_bd_pins psAxiInterconnect/S00_ARESETN] [get_bd_pins psInterruptController/s_axi_aresetn] [get_bd_pins subprocessorClk/s_axi_aresetn]
-  connect_bd_net -net S01_ARESETN_1 [get_bd_pins cortexm3/m_axi_aresetn] [get_bd_pins irqAxiInterconnect/S01_ARESETN]
-  connect_bd_net -net irq [get_bd_pins cortexm3/irq] [get_bd_pins irqConcat/In0]
+  connect_bd_net -net S00_ARESETN_1 [get_bd_pins irqAxiInterconnect/M00_ARESETN] [get_bd_pins irqAxiInterconnect/S00_ARESETN] [get_bd_pins porReset/peripheral_aresetn] [get_bd_pins processor/s_axi_aresetn] [get_bd_pins psAxiInterconnect/M00_ARESETN] [get_bd_pins psAxiInterconnect/M01_ARESETN] [get_bd_pins psAxiInterconnect/S00_ARESETN] [get_bd_pins psInterruptController/s_axi_aresetn] [get_bd_pins subprocessorClk/s_axi_aresetn]
+  set_property -dict [ list \
+HDL_ATTRIBUTE.DEBUG {true} \
+ ] [get_bd_nets S00_ARESETN_1]
+  connect_bd_net -net S01_ARESETN_1 [get_bd_pins irqAxiInterconnect/S01_ARESETN] [get_bd_pins processor/m_axi_aresetn]
+  connect_bd_net -net irq [get_bd_pins irqConcat/In0] [get_bd_pins processor/irq]
   connect_bd_net -net porReset_interconnect_aresetn [get_bd_pins irqAxiInterconnect/ARESETN] [get_bd_pins porReset/interconnect_aresetn] [get_bd_pins psAxiInterconnect/ARESETN]
-  connect_bd_net -net por_resetn [get_bd_pins cortexm3/por_resetn] [get_bd_pins porReset/ext_reset_in] [get_bd_pins processing_system7_0/FCLK_RESET0_N]
+  connect_bd_net -net por_resetn [get_bd_pins porReset/ext_reset_in] [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins processor/por_resetn]
+  set_property -dict [ list \
+HDL_ATTRIBUTE.DEBUG {true} \
+ ] [get_bd_nets por_resetn]
   connect_bd_net -net processing_system7_0_GPIO_O [get_bd_pins processing_system7_0/GPIO_O] [get_bd_pins resetSlice/Din]
   connect_bd_net -net psirq [get_bd_pins processing_system7_0/IRQ_F2P] [get_bd_pins psInterruptController/irq]
-  connect_bd_net -net riscv_resetn [get_bd_pins cortexm3/riscv_resetn] [get_bd_pins resetSlice/Dout]
-  connect_bd_net -net subprocessorClk [get_bd_pins cortexm3/riscv_clk] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK] [get_bd_pins subprocessorClk/clk_out1]
-  connect_bd_net -net tutorialProcessor_clock [get_bd_pins cortexm3/m_axi_aclk] [get_bd_pins irqAxiInterconnect/S01_ACLK]
+  connect_bd_net -net riscv_resetn [get_bd_pins processor/riscv_resetn] [get_bd_pins resetSlice/Dout]
+  connect_bd_net -net subprocessorClk [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK] [get_bd_pins processor/riscv_clk] [get_bd_pins subprocessorClk/clk_out1]
+  connect_bd_net -net tutorialProcessor_clock [get_bd_pins irqAxiInterconnect/S01_ACLK] [get_bd_pins processor/m_axi_aclk]
   connect_bd_net -net xlconcat_0_dout [get_bd_pins irqConcat/dout] [get_bd_pins psInterruptController/intr]
 
   # Create address segments
-  create_bd_addr_seg -range 0x00010000 -offset 0x40010000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs cortexm3/psBramController/S_AXI/Mem0] SEG_psBramController_Mem0
+  create_bd_addr_seg -range 0x00010000 -offset 0x40010000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs processor/psBramController/S_AXI/Mem0] SEG_psBramController_Mem0
   create_bd_addr_seg -range 0x00001000 -offset 0x80000000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs psInterruptController/S_AXI/Reg] SEG_psInterruptController_Reg
   create_bd_addr_seg -range 0x00001000 -offset 0x40001000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs subprocessorClk/s_axi_lite/Reg] SEG_subprocessorClk_Reg
-  create_bd_addr_seg -range 0x00001000 -offset 0x80000000 [get_bd_addr_spaces cortexm3/cortexm3/CM3_SYS_AXI3] [get_bd_addr_segs psInterruptController/S_AXI/Reg] SEG_psInterruptController_Reg
-  create_bd_addr_seg -range 0x00001000 -offset 0x80000000 [get_bd_addr_spaces cortexm3/cortexm3/CM3_CODE_AXI3] [get_bd_addr_segs psInterruptController/S_AXI/Reg] SEG_psInterruptController_Reg
-  create_bd_addr_seg -range 0x00010000 -offset 0x40010000 [get_bd_addr_spaces cortexm3/cortexm3/CM3_SYS_AXI3] [get_bd_addr_segs cortexm3/spBramController/S_AXI/Mem0] SEG_spBramController_Mem0
-  create_bd_addr_seg -range 0x00010000 -offset 0x40010000 [get_bd_addr_spaces cortexm3/cortexm3/CM3_CODE_AXI3] [get_bd_addr_segs cortexm3/spBramController/S_AXI/Mem0] SEG_spBramController_Mem0
+  create_bd_addr_seg -range 0x00001000 -offset 0x80000000 [get_bd_addr_spaces processor/cortexm3/CM3_SYS_AXI3] [get_bd_addr_segs psInterruptController/S_AXI/Reg] SEG_psInterruptController_Reg
+  create_bd_addr_seg -range 0x00001000 -offset 0x80000000 [get_bd_addr_spaces processor/cortexm3/CM3_CODE_AXI3] [get_bd_addr_segs psInterruptController/S_AXI/Reg] SEG_psInterruptController_Reg
+  create_bd_addr_seg -range 0x00010000 -offset 0x00000000 [get_bd_addr_spaces processor/cortexm3/CM3_SYS_AXI3] [get_bd_addr_segs processor/spBramController/S_AXI/Mem0] SEG_spBramController_Mem0
+  create_bd_addr_seg -range 0x00010000 -offset 0x00000000 [get_bd_addr_spaces processor/cortexm3/CM3_CODE_AXI3] [get_bd_addr_segs processor/spBramController/S_AXI/Mem0] SEG_spBramController_Mem0
 
 
   # Restore current instance
@@ -1259,7 +1265,7 @@ proc available_tcl_procs { } {
    puts "##################################################################"
    puts "# Available Tcl procedures to recreate hierarchical blocks:"
    puts "#"
-   puts "#    create_hier_cell_cortexm3_1 parentCell nameHier"
+   puts "#    create_hier_cell_processor parentCell nameHier"
    puts "#    create_root_design"
    puts "#"
    puts "#"
